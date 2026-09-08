3459b059f70cc9cd80d76b382eb16c20e36bccdb
kela01-dev <kelauner69@gmail.com>
2026-09-08 17:15:32 +0700

    Update rules.py

--- core/signals/rules.py
+++ core/signals/rules.py
@@ -1,1 +1,6 @@
-
+def check_stop_loss_take_profit(entry_price, current_price, stop_loss_pct=0.02, take_profit_pct=0.05):
+    if current_price <= entry_price * (1 - stop_loss_pct):
+        return 'STOP_LOSS'
+    elif current_price >= entry_price * (1 + take_profit_pct):
+        return 'TAKE_PROFIT'
+    return None
