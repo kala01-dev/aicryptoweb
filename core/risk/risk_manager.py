9b2b0b3e871c5375c91b258365bfe54b6db4b898
kela01-dev <kelauner69@gmail.com>
2026-09-08 17:11:33 +0700

    Update risk_manager.py

--- core/risk/risk_manager.py
+++ core/risk/risk_manager.py
@@ -1,1 +1,8 @@
-
+def get_stop_loss_take_profit(entry_price, atr_value, side='buy', sl_mult=2.0, rr_ratio=2.0):
+    if side == 'buy':
+        stop_loss = entry_price - sl_mult * atr_value
+        take_profit = entry_price + sl_mult * rr_ratio * atr_value
+    else:
+        stop_loss = entry_price + sl_mult * atr_value
+        take_profit = entry_price - sl_mult * rr_ratio * atr_value
+    return stop_loss, take_profit
