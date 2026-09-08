dd51889e20bd8b808b6b285db79daeeebd8a2380
kela01-dev <kelauner69@gmail.com>
2026-09-08 17:15:18 +0700

    Update filters.py

--- core/signals/filters.py
+++ core/signals/filters.py
@@ -1,1 +1,5 @@
+def filter_by_min_volume(df_tickers, min_volume=1_000_000):
+    return df_tickers[df_tickers['quoteVolume'] >= min_volume]
 
+def filter_by_confidence(signals, min_confidence=60):
+    return [s for s in signals if s['total_score'] >= min_confidence]
