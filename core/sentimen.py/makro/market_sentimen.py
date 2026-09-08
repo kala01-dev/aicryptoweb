fdd88603d54da37f4e11687a19badce0858f8a30
kela01-dev <kelauner69@gmail.com>
2026-09-08 17:14:12 +0700

    Update market_sentimen.py

--- core/sentimen.py/makro/market_sentimen.py
+++ core/sentimen.py/makro/market_sentimen.py
@@ -1,1 +1,15 @@
+"""Indikator sentimen makro pasar."""
+def get_fear_greed_index():
+    try:
+        import requests
+        resp = requests.get('https://api.alternative.me/fng/', timeout=5)
+        if resp.status_code == 200:
+            data = resp.json()
+            return int(data['data'][0]['value'])  # 0-100
+    except:
+        pass
+    return 50  # netral
 
+def get_macro_sentiment_score():
+    fng = get_fear_greed_index()
+    return fng
