74d09cc814520cd1e94415fc1bb55d861734a967
kela01-dev <kelauner69@gmail.com>
2026-09-08 17:15:05 +0700

    Update scorer.py

--- core/signals/scorer.py
+++ core/signals/scorer.py
@@ -1,1 +1,4 @@
-
+def combine_scores(tech_score, fund_score, news_score, ml_prob, weights=(0.5,0.3,0.2)):
+    base_score = weights[0]*tech_score + weights[1]*fund_score + weights[2]*news_score
+    total = base_score * 0.7 + (ml_prob * 100) * 0.3
+    return total
